9611d21d3cd6ab23c8a0cfa764bea4ca05625a50
Fernando Medina Corey <fmcorey@gmail.com>
2016-05-22 18:55:07 -0400

    Update comments in sparrow.py
    Changed comments to reflect changes to code.

--- sparrow.py
+++ sparrow.py
@@ -2,15 +2,11 @@
 import json
 from twython import Twython
 
-#These values are all pulled from a file called 'config.ini'
-#You can call yours myawesomebotconfig.ini or whatever else!
-#Just remember to change it here
-
+#This opens 'creds.json' and turns json into a Python array
 with open('creds.json') as f:
     credentials = json.loads(f.read())
 
-#SECURE YOUR CONFIG FILE - Don't put it in source code
-
+#This creates the Python Twitter client and uses our credentials
 twitter = Twython(credentials["consumer_key"],
                   credentials["consumer_secret"],
                   credentials["access_token_key"],
