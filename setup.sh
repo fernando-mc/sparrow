be84461a42af862a776fee4ddb84b04a3db3da72
Fernando Medina Corey <fernando@curalate.com>
2016-11-30 09:35:50 -0500

    Updated setup

--- setup.sh
+++ setup.sh
@@ -2,27 +2,31 @@
 
 # Create and initialize a Python Virtual Environment
 echo "Creating virtual env - .env"
-virtualenv .env && echo "source virtual env - .env" && source .env/bin/activate
+virtualenv .env
 
-# create a directory to put things in
-echo "Creating 'setup' dir"
+echo "sourcing virtual env - .env"
+source .env/bin/activate
+
+# Create a directory to put things in
+echo "Creating 'setup' directory"
 mkdir setup
 
-# Move that stuff into setup
+# Move the relevant files into setup directory
 echo "Moving creds.json and function file(s) to setup dir"
 cp creds.json setup/creds.json
 cp sparrow_*.py setup/
 cd ./setup
 
 # Install requirements 
-echo "pip installing requirements"
+echo "pip installing requirements from requirements file in target directory"
 pip install -r ../requirements.txt -t .
 
 # Prepares the deployment package
 echo "Zipping package"
 zip -r ../package.zip ./* 
 
-# Remove the dir used
+# Remove the setup directory used
+echo "Removing setup directory and virtual environment"
 cd ..
 rm -rf ./setup
 deactivate
