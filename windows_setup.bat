e9e518a7e744b6dd09e35cf83f9f5eac2f596684
Fernando Medina Corey <fernando@curalate.com>
2017-01-08 01:30:06 -0500

    Add windows setup script

---
+++ windows_setup.bat
@@ -0,0 +1,40 @@
+:: Create and initialize a Python Virtual Environment
+echo "Creating virtual env - .env"
+virtualenv .env
+
+echo "starting virtualenv - .env"
+call .env\Scripts\activate.bat
+
+:: Create a directory to put things in
+echo "Creating 'setup' directory"
+mkdir setup
+
+:: Move the relevant files into setup directory
+echo "Moving creds.json and function file(s) to setup dir"
+xcopy .\creds.json .\setup\creds.json /Q /R /Y
+F
+xcopy .\sparrow_kms.py setup\ /Q /R /Y
+xcopy .\sparrow_nokms.py setup\ /Q /R /Y
+cd .\setup
+
+:: Install requirements 
+echo "pip installing requirements from requirements file in target directory"
+pip install -r ..\requirements.txt -t .
+
+:: Prepares the deployment package
+echo "Setting up your 7zip PATH - This assumes the installation location of 7zip is in C:\Program Files\7-Zip\"
+set PATH=%PATH%;C:\Program Files\7-Zip\
+
+echo "Zipping package"
+7z a -r ..\package.zip .\* 
+
+:: Remove the setup directory used
+echo "Removing setup directory and virtual environment"
+cd ..
+rd /Q /S .\setup
+call .env\Scripts\deactivate.bat
+rd /Q /S .\.env
+
+:: changing dirs back to dir from before
+echo "Opening folder containg function package - 'package.zip'"
+explorer .
