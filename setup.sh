701fa6c94193b951fb709ae7e8f937bf740c8436
Fernando Medina Corey <fmcorey@gmail.com>
2017-03-27 20:38:18 -0700

    remove force in setup cleanup process

--- setup.sh
+++ setup.sh
@@ -28,9 +28,9 @@ zip -r ../package.zip ./*
 # Remove the setup directory used
 echo "Removing setup directory and virtual environment"
 cd ..
-rm -rf ./setup
+rm -r ./setup
 deactivate
-rm -rf ./.env
+rm -r ./.env
 # changing dirs back to dir from before
 echo "Opening folder containg function package - 'package.zip'"
 open .
